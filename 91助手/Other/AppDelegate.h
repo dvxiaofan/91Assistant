1c7f1704db599932af77fda3b96f52d208bfa590
xiaofan <1353815633@qq.com>
2016-05-30 10:17:09 +0800

    初步搭建框架

---
+++ 91助手/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  91助手
+//
+//  Created by 小烦 on 16/5/27.
+//  Copyright © 2016年 YK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 91助手/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  91助手
+//
+//  Created by 小烦 on 16/5/27.
+//  Copyright © 2016年 YK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
